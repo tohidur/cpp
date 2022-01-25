01213d2490a2560f756613f819bc58ddeaddcb1b
tohidur <tohidur@urbanpiper.com>
2022-01-25 19:06:10 +0530

    merge two binary tress iterative approach

--- problems/leet_code/random_problems/10_e_merge_two_binary_trees.cpp
+++ problems/leet_code/random_problems/10_e_merge_two_binary_trees.cpp
@@ -21,6 +21,7 @@ public:
     Node(int _data) { data=_data; }
 };
 
+
 Node* merge(Node* r1, Node* r2) {
     if (r2 == nullptr)
         return r1;
@@ -33,6 +34,61 @@ Node* merge(Node* r1, Node* r2) {
 
     return r1;
 }
+
+
+struct snode
+{
+    Node *l, *r;
+};
+
+
+Node* merge_itr(Node* t1, Node* t2)
+{
+    if (! t1)
+        return t2;
+    if (! t2)
+        return t1;
+
+    stack<snode> s;
+    snode temp;
+    temp.l = t1;
+    temp.r = t2;
+    s.push(temp);
+
+    snode n;
+    while (!s.empty())
+    {
+        n = s.top();
+        s.pop();
+        if (n.l == nullptr || n.r == nullptr)
+            continue;
+
+        n.l->data += n.r->data;
+
+        if (n.l->left == nullptr)
+            n.l->left = n.r->left;
+        else
+        {
+            snode t;
+            t.l = n.l->left;
+            t.r = n.r->left;
+            s.push(t);
+        }
+
+        if (n.l->right == nullptr)
+            n.l->right = n.r->right;
+        else
+        {
+            snode t;
+            t.l = n.l->right;
+            t.r = n.r->right;
+            s.push(t);
+        }
+    }
+
+    return t1;
+}
+
 
 void printInOrder(Node* r) {
     stack<Node*> st;
@@ -51,6 +107,7 @@ void printInOrder(Node* r) {
     }
 }
 
+
 int main()
 {
     Node* r1 = new Node(3);
@@ -64,7 +121,14 @@ int main()
     r2->left->right = new Node(4);
     r2->right->right = new Node(9);
 
-    Node* merged_tree = merge(r1,r2);
-    printInOrder(merged_tree);
+    cout << "Recursive Solution" << endl;
+    Node* merged_rec = merge(r1,r2);
+    printInOrder(merged_rec);
+    cout << endl << endl;
+
+    cout << "Iterative Solution" << endl;
+    Node* merged_itr = merge_itr(r1,r2);
+    printInOrder(merged_itr);
+    cout << endl << endl;
 }
 
